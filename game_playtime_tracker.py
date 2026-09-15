0506a2baf47c35197308843eab71f75fcf1a336a
Joe Widdifield <joewidd94@gmail.com>
2026-09-15 16:11:49 -0400

    Setting up user input and testing from examples from the book

--- game_playtime_tracker.py
+++ game_playtime_tracker.py
@@ -5,3 +5,16 @@ This will be the program file
 tbd: 'any info about starter code/resources used'
 9-11-2026
 """
+
+##username = input("Please enter your username: ")
+active = True
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' or 'q' to end the program. "
+
+message = ""
+while active:
+    message = input(prompt)
+    if message == 'quit' or message == 'q':
+        active = False
+    else:
+        print(message)
