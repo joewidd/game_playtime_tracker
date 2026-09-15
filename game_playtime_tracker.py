d2e3edc377aa92c7bc012f88f7493bd9d09a43c8
Joe Widdifield <joewidd94@gmail.com>
2026-09-15 16:24:10 -0400

    formatting

--- game_playtime_tracker.py
+++ game_playtime_tracker.py
@@ -5,12 +5,14 @@ This will be the program file
 tbd: 'any info about starter code/resources used'
 9-11-2026
 """
+
 GAMES = ("Hades", "Stardew Valley", "Elden Ring", "Deadlock", "Monster Hunter", "Resident Evil", "Kingdom Hearts", "Bloodborne", "Pokemon", "Persona", "Overwatch", "Tekken", "Gex")
 ##username = input("Please enter your username: ")
+
+
 active = True
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' or 'q' to end the program. "
-
 message = ""
 while active:
     message = input(prompt)
