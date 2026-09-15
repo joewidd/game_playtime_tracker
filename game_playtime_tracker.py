a151a1e66680e255ea3eb5312ecd2f7fe1b437ed
Joe Widdifield <joewidd94@gmail.com>
2026-09-15 16:21:09 -0400

    Adding an immutable list of games

--- game_playtime_tracker.py
+++ game_playtime_tracker.py
@@ -5,7 +5,7 @@ This will be the program file
 tbd: 'any info about starter code/resources used'
 9-11-2026
 """
-
+GAMES = ("Hades", "Stardew Valley", "Elden Ring", "Deadlock", "Monster Hunter", "Resident Evil", "Kingdom Hearts", "Bloodborne", "Pokemon", "Persona", "Overwatch", "Tekken", "Gex")
 ##username = input("Please enter your username: ")
 active = True
 prompt = "\nTell me something, and I will repeat it back to you:"
