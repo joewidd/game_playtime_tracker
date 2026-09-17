e7dd24d7d3bf74843a9c3da8a8571362eb52def1
Joe Widdifield <joewidd94@gmail.com>
2026-09-17 09:07:28 -0400

    Added a function to contain the user input section and added the use of the GAMES list to match which game the user is picking

--- game_playtime_tracker.py
+++ game_playtime_tracker.py
@@ -1,22 +1,37 @@
 """
 Game Playtime Tracker
 Joe Widdifield
-This will be the program file
-tbd: 'any info about starter code/resources used'
+This will be the main program file
 9-11-2026
 """
 
 GAMES = ("Hades", "Stardew Valley", "Elden Ring", "Deadlock", "Monster Hunter", "Resident Evil", "Kingdom Hearts", "Bloodborne", "Pokemon", "Persona", "Overwatch", "Tekken", "Gex")
 ##username = input("Please enter your username: ")
 
+def collect_user_input():
+    game_prompt = "\nTell me the game you played from the games listed"
+    game_prompt += f"\n{GAMES}"
+    game_prompt += "\nEnter 'quit' or 'q' to end the program: "
+    hours_prompt = "\nTell me how many hours you played"
+    hours_prompt += "\nEnter 'quit' or 'q' to end the program: "
+    game = ""
+    hours = ""
+    while True:
 
-active = True
-prompt = "\nTell me something, and I will repeat it back to you:"
-prompt += "\nEnter 'quit' or 'q' to end the program. "
-message = ""
-while active:
-    message = input(prompt)
-    if message == 'quit' or message == 'q':
-        active = False
-    else:
-        print(message)
+        game = input(game_prompt)
+        if game == 'quit' or game == 'q':
+            break
+        else:
+            for i in GAMES:
+                if game == i:
+                    print(f"\n{game}")
+                    break
+
+        hours = input(hours_prompt)
+        if hours == 'quit' or hours == 'q':
+            break
+        else:
+            print(f"\n{hours} hours played")
+            break
+
+collect_user_input()
