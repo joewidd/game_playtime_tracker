bfa150650a554d54e58527663dc217c627cbaec6
Joe Widdifield <joewidd94@gmail.com>
2026-09-21 06:43:02 -0400

    removed some unused code and edited how the output looks

--- game_playtime_tracker.py
+++ game_playtime_tracker.py
@@ -6,9 +6,8 @@ This will be the main program file
 """
 
 GAMES = ("Hades", "Stardew Valley", "Elden Ring", "Deadlock", "Monster Hunter", "Resident Evil", "Kingdom Hearts", "Bloodborne", "Pokemon", "Persona", "Overwatch", "Tekken", "Gex")
-##username = input("Please enter your username: ")
 
-def collect_user_input():
+def get_game_playtime():
     game_prompt = "\nTell me the game you played from the games listed"
     game_prompt += f"\n{GAMES}"
     game_prompt += "\nEnter 'quit' or 'q' to end the program: "
@@ -24,14 +23,14 @@ def collect_user_input():
         else:
             for i in GAMES:
                 if game == i:
-                    print(f"\n{game}")
+                    print(f"\nYou've selected{game}")
                     break
 
         hours = input(hours_prompt)
         if hours == 'quit' or hours == 'q':
             break
         else:
-            print(f"\n{hours} hours played")
+            print(f"\n{hours} hours played in {game}")
             break
 
-collect_user_input()
+get_game_playtime()
